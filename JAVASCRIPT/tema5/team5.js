be5eee2e4317ff22fc74bb5c251ceb9662813b16
Victor Rodriguez Martin <vicrodri@c2r4s1.42malaga.com>
2023-06-26 14:47:56 +0200

    add tema5 javascript

---
+++ JAVASCRIPT/tema5/team5.js
@@ -0,0 +1,10 @@
+let lenghtcm = 175
+let lenghtm = lenghtcm / 100
+let weight = 68.5
+let lenghtred = lenghtm.toFixed(1)
+let weightred = Math.floor(weight)
+let numMax = Number.MAX_VALUE;
+let lMax = false;
+if ((numMax+1)===numMax){
+    lMax=true;
+}
