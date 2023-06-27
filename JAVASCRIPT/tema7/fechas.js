9f58495b64cb007435e6c3108a399110c09839d2
Victor Rodriguez Martin <vicrodri@c2r4s2.42malaga.com>
2023-06-27 15:34:12 +0200

    add tema7.3 javascript

---
+++ JAVASCRIPT/tema7/fechas.js
@@ -0,0 +1,6 @@
+const hoy = new Date();
+const birthday = new Date(1993,9,20);
+const tarde = (hoy > birthday)
+const diaNac = birthday.getDate();
+const mesNac = birthday.getMonth()+1;
+const annoNac = birthday.getFullYear();
